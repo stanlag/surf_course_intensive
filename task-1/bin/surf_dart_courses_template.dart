2f08f413d783e3ff7d9e6940743000b462563251
stanlag <stanlag@mail.ru>
2024-03-03 19:19:21 +0300

    task 1

--- task-1/bin/surf_dart_courses_template.dart
+++ task-1/bin/surf_dart_courses_template.dart
@@ -1,5 +1,5 @@
 import 'package:surf_dart_courses_template/surf_dart_courses_template.dart' as surf_dart_courses_template;
-
+// task-1/bin/surf_dart_courses_template.dart
 void main(List<String> arguments) {
   print('Hello world: ${surf_dart_courses_template.calculate()}!');
 }
